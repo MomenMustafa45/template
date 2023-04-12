a270f46bd42d3ef44a1fe92c08e3f60f029c9139
MomenMustafa45 <momenkara2@gmail.com>
2023-04-12 13:54:22 +0200

    fixed the code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Design from './pages/Design/Design';
-import Footer from './pages/Footer/Footer';
 import Home from './pages/Home/Home';
-import Portfolio from './pages/Portfolio/Portfolio';
-import Pricing from './pages/Pricing/Pricing';
-import Services from './pages/Services/Services';
-import Skills from './pages/Skills/Skills';
-import Stats from './pages/Stats/Stats';
-import VideoPage from './pages/Video/VideoPage';
-import ScrollButton from './UI/scroll-btn/ScrollButton';
+
+
 
 
 function App() {
@@ -20,18 +11,16 @@ function App() {
 
   return (
     <>
-      <ScrollButton />
-      <Home />
-      <Services />
-      <Design />
-      <Portfolio />
-      <VideoPage />
-      <About />
-      <Stats />
-      <Skills />
-      <Pricing />
-      <Contact />
-      <Footer />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+
+
+      </Routes>
+
+
+
     </>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,92 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "../../components/Navbar";
-import { HashLink } from "react-router-hash-link";
-import "../../styles/home.css";
+import React from "react";
+import LandingPage from "../LandingPage/LandingPage";
+import Design from "../Design/Design";
+import Portfolio from "../Portfolio/Portfolio";
+import VideoPage from "../Video/VideoPage";
+import About from "../About/About";
+import Stats from "../Stats/Stats";
+import Skills from "../Skills/Skills";
+import Pricing from "../Pricing/Pricing";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+import ScrollButton from "../../UI/scroll-btn/ScrollButton";
 
 const Home = () => {
-  const [linksOpened, setLinksOpened] = useState(false);
-  const [imgNum, setImgNum] = useState("1");
-  const bulletsNum = [1, 2, 3];
-
-  const linksHandler = () => {
-    setLinksOpened(!linksOpened);
-  };
-
-  useEffect(() => {
-    const intervalImg = setInterval(() => {
-      imgNum === 3 ? setImgNum(1) : setImgNum((prev) => +prev + 1);
-    }, 4000);
-    return () => {
-      clearInterval(intervalImg);
-    };
-  }, [imgNum]);
-
-  const changeImageHandler = (direction) => {
-    switch (direction) {
-      case "right":
-        imgNum === 3 ? setImgNum(1) : setImgNum((prev) => +prev + 1);
-        break;
-      case "left":
-        imgNum === 1 ? setImgNum(3) : setImgNum((prev) => +prev - 1);
-        break;
-      default:
-    }
-  };
-
   return (
-    <header className={`home-parent home-parent-bg${imgNum}`} id="home">
-      <Navbar queryLinksOpened={linksHandler} />
-      <div
-        className="right-arrow"
-        onClick={() => changeImageHandler("right")}
-      ></div>
-      <div
-        className="left-arrow"
-        onClick={() => changeImageHandler("left")}
-      ></div>
-      <div className="bullets">
-        {bulletsNum.map((e) => {
-          return (
-            <span className={e === +imgNum ? "active" : ""} key={e}></span>
-          );
-        })}
-      </div>
-      <div className={`query-list ${linksOpened ? "" : "query-list-close"}`}>
-        <ul>
-          <li>
-            <HashLink href="">Home</HashLink>
-          </li>
-          <li>
-            <HashLink href="">Services</HashLink>
-          </li>
-          <li>
-            <HashLink href="">Portfolio</HashLink>
-          </li>
-          <li>
-            <HashLink href="">About</HashLink>
-          </li>
-          <li>
-            <HashLink href="">Pricing</HashLink>
-          </li>
-          <li>
-            <HashLink href="">Contact</HashLink>
-          </li>
-        </ul>
-      </div>
-      <div className="landing-container">
-        <div className="landing-text">
-          <h2>
-            Hello World! <br /> We Are Kasper We Make Art.
-          </h2>
-          <p>
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
-            Curabitur aliquet quam. Accumsan id imperdiet et, porttitor at sem.
-            Mauris blandit aliquet elit, eget tincidunt.
-          </p>
-        </div>
-      </div>
-    </header>
+    <>
+      <ScrollButton />
+      <LandingPage />
+      <Design />
+      <Portfolio />
+      <VideoPage />
+      <About />
+      <Stats />
+      <Skills />
+      <Pricing />
+      <Contact />
+      <Footer />
+    </>
   );
 };
 
