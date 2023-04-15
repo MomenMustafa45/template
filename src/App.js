b45d809f8b1aee92638d1beceb95f521783eb471
MomenMustafa45 <momenkara2@gmail.com>
2023-04-15 20:36:12 +0200

    contact button fixed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ import Home from './pages/Home/Home';
 
 function App() {
 
+  function Phone(serial, name, price) {
+    this.serial = serial
+    this.name = name
+    this.price = price
+    this.newName = (newName) => {
+      this.name = newName
+    }
+  }
+
+  const phone1 = new Phone(123, 'samsung', 3000)
+  let str = 'hell'
+  phone1.newName('oppo')
+  console.log(phone1.name);
+  phone1.age = 24
+  console.log(phone1);
+
 
   return (
     <>
       <Routes>
         <Route path='/template' element={<Home />} />
       </Routes>
-
-
-
     </>
   );
 }
